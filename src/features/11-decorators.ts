ed16deaeb4e512e2732a422150ce4634ee2477ff
Felix Eklöf <felixeklof@icloud.com>
2023-07-24 23:48:33 +0200

    fix sorting of files

--- src/features/11-decorators.ts
+++ src/features/11-decorators.ts
@@ -1,6 +1,6 @@
 import consola from 'consola';
 import ky, { type KyResponse } from 'ky';
-import { type JSONValue } from './8-json-value.js';
+import { type JSONValue } from './08-json-value.js';
 
 // Example type representing a http requests.
 interface HttpRequest {
